35a36b89734daf782b61354a4c9e38ae32a68f71
MatthieuGANNE <matthieu.ganne@gmail.com>
2016-05-12 16:27:45 +0200

    Update sauvegarde.php

--- sauvegarde.php
+++ sauvegarde.php
@@ -1,6 +1,6 @@
 <?php 
 function sauvegarde(){	
-         $remplis=true;
+         $remplis=true; // on verifie si les champs sont vides et si ils correspondent au format attendu
          $valide=true;
 
          if (!(isset($_POST["nom"]))|| $_POST["nom"]=="")
@@ -9,19 +9,19 @@ function sauvegarde(){
 			$remplis=false;
          if (!(isset($_POST["mail"])) || $_POST["mail"]=="")
 			$remplis=false;
-         if ($_POST["nom"]!=htmlspecialchars($_POST["nom"]) )
+         if ($_POST["nom"]!=htmlspecialchars($_POST["nom"]) ) // on protège aux inculsions html
 			$valide=false;
          if ($_POST["prenom"]!=htmlspecialchars($_POST["prenom"]))
 			$valide=false;	 
          if ($_POST["mail"]!=htmlspecialchars($_POST["mail"]))
-         $valide=false;
+         	$valide=false;
 	
 
 
          if (!$remplis){
-         return "remplis mieux <br/> <a href=\"index.php?page=inscription\">Retourner au form</a>";
+         return "remplis mieux <br/> <a href=\"index.php?page=inscription\">Retourner au form</a>"; // Message d'erreur
          } else if(!$valide){
-         return "c'est pas valide <br/> <a href=\"index.php?page=inscription\">Retourner au form</a>";
+         return "c'est pas valide <br/> <a href=\"index.php?page=inscription\">Retourner au form</a>"; // Message d'erreur
       } else {
    
 			$server= "localhost";
@@ -35,12 +35,12 @@ function sauvegarde(){
 			if (!mysqli_select_db($connexion,$base)){
 				echo "pas de base"; exit;
 			}
-			if(!isset($_SESSION["nom"])){
+			if(!isset($_SESSION["nom"])){ // Inscription
 				$_SESSION["rank"]=0;
 				$pass=md5("$_POST[mdp1]");
 				$req="INSERT INTO users (nom, prenom, pseudo, mail,mdp,rank) VALUES('$_POST[nom]','$_POST[prenom]','$_POST[pseudo]','$_POST[mail]','$pass',0)";
 				mysqli_query($connexion,$req) OR die(mysqli_sqlstate($connexion));
-			} else {
+			} else { // Modification du Profil
 				$req="UPDATE users 
 				SET nom = '$_POST[nom]', 
 				prenom = '$_POST[prenom]', 
@@ -49,7 +49,7 @@ function sauvegarde(){
 				WHERE mail='$_SESSION[mail]'";
 				mysqli_query($connexion,$req) OR die(mysqli_sqlstate($connexion));
 			}		
-			$_SESSION["nom"]=$_POST["nom"];
+			$_SESSION["nom"]=$_POST["nom"]; // On mets à jour la session 
 			$_SESSION["prenom"]= $_POST["prenom"];
 			$_SESSION["pseudo"] = $_POST["pseudo"];
 			$_SESSION["mail"]= $_POST["mail"];
