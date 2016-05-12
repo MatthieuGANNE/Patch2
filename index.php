58e258f78e8a5709010fff969e147c5ec5a12c82
MatthieuGANNE <matthieu.ganne@gmail.com>
2016-05-12 16:19:31 +0200

    Update index.php

--- index.php
+++ index.php
@@ -11,20 +11,19 @@
   <body>
   <span id=haut_de_page></span>
     <?php
-       include "permanent.php";
-       include "inscription.php";
-       include "sauvegarde.php";
-       include "accueil.php";
-       include "connexion.php";
-       include "article.php";
-       include "identification.php";
-	   include "commentaire.php";
-	   include "moderation.php";
+        include "permanent.php"; // inclusion des bibliothèques de fonctions
+        include "inscription.php";
+        include "sauvegarde.php";
+        include "accueil.php";
+        include "connexion.php";
+        include "article.php";
+        include "identification.php";
+        include "commentaire.php";
+	include "moderation.php";
        ?>
     <header>
       <?php
-         echo afficher_header();
-		 
+         echo afficher_header(); // affichage du header sur chaque page 
          ?>
     </header>
 	
@@ -33,56 +32,61 @@
 	$user="root";
 	$base="blog";
 	$password="";
-	$connexion = mysqli_connect($server, $user, $password,$base);
+	$connexion = mysqli_connect($server, $user, $password,$base); // Connection à la base de donnée
 	if (!$connexion){
 		echo "connexion none"; exit;
 	}
 	if (!mysqli_select_db($connexion,$base)){
 		echo "pas de base"; exit;
 	}
-       if (!isset($_GET["page"])){
+	
+       if (!isset($_GET["page"])){ // si l'utilisateur n'a pas demandé de page en particulier on affiche la page d'accueil
 			echo accueil_article();
        } else {
-       if ($_GET["page"]=="inscription" || $_GET["page"]=="modifier")
+       	
+       if ($_GET["page"]=="inscription" || $_GET["page"]=="modifier") // Demande d'inscription ou de modification de profil
           echo inscription();
-       if ($_GET["page"]=="sauvegarde")
+       if ($_GET["page"]=="sauvegarde") // Fonction interne qui permet de sauvegarder les changements des informations de l'utilisateur
           echo sauvegarde();
        if ($_GET["page"]=="preferences")
           echo preferences();
-       if ($_GET["page"]=="deconnexion"){      
-           session_unset(); // Cette fonction marche alors que unset($_SESSION) marchait pas
+       if ($_GET["page"]=="deconnexion"){ // on enlève les cookies qui permettait de facliter la navigation et on redirige l'utilisateur vers l'accueil
+           session_unset(); 
            header("Location: index.php");
        }
-       if ($_GET["page"]=="connexion")
+       if ($_GET["page"]=="connexion") // Page de connexion
            echo connexion();
        
-          
-		if($_GET["page"]=="article_form"){
-				article_form();
-			echo"<br>";
-		}	
-		if($_GET["page"]=="sauvegarde_article")
-				article_sauvegarde();
+       if($_GET["page"]=="article_form"){ // Page d'édition d'un article
+		article_form();
+		echo"<br>";
+	}	
+	
+	if($_GET["page"]=="sauvegarde_article")// Fonction interne qui sauvegarde les articles et les changements sur l'article
+		article_sauvegarde();
 			
-		if($_GET["page"]=="consulter"){
-			echo afficher_article($_GET["id"]);
-			article_commentaire($_GET["id"]);
-		}	
+	if($_GET["page"]=="consulter"){ // Consultation d'un article en particulier avec ces commentaires
+		echo afficher_article($_GET["id"]);
+		article_commentaire($_GET["id"]);
+	}	
 		
-		if($_GET["page"] == "sauvegarde_commentaire"){
-			sauvegarde_commentaire();
-		}
+	if($_GET["page"] == "sauvegarde_commentaire"){ // Fonction interne qui permet de sauvegarder un nouveau commentaire
+		sauvegarde_commentaire();
+	}
 		
-	   if ($_GET["page"]=="identification"){
-          echo identification();
+	if ($_GET["page"]=="identification"){
+       		echo identification();
        }
-	    if ($_GET["page"]=="ancien_article"){
-          echo afficher_titre_article();
+       
+	if ($_GET["page"]=="ancien_article"){ // Consultation de la liste de tous les articles
+          	echo afficher_titre_article();
        }         
-	   if ($_GET["page"]=="mes_articles"){
+	
+	if ($_GET["page"]=="mes_articles"){ // Consultation des articles publiés par l'utilisateur
 		   echo afficher_titre_utilisateur($_SESSION['mail']);
-	   }
-	   if ($_GET["page"]=="modifier_page"){
+	}
+	
+	if ($_GET["page"]=="modifier_page"){
 		    $req= "SELECT pseudo
 					FROM users
 					WHERE id = (SELECT id_users FROM article WHERE id=$_GET[id])";
@@ -94,6 +98,7 @@
 			echo redirection();
 		}
 	   }
+	   
 	   if ($_GET["page"]=="modifier_article"){
 		   $req= "SELECT pseudo
 					FROM users
@@ -106,6 +111,7 @@
 			echo redirection();
 		}
 	   }
+	   
 	   if ($_GET["page"]=="supprimer_article"){
 		   $req= "SELECT pseudo
 					FROM users
@@ -118,6 +124,7 @@
 			echo redirection();
 		}
 	   }
+	   
 	   if ($_GET["page"]=="moderation_utilisateur"){
 		   if (isset($_SESSION["rank"]) && $_SESSION["rank"]==1){
 		   echo page_moderation();
@@ -125,6 +132,7 @@
 				echo redirection();
 			}
 	   }
+	   
 	   if ($_GET["page"]=="article_utilisateur"){
 		   $req= "SELECT mail
 				  FROM users
@@ -132,7 +140,8 @@
 		$resultat = mysqli_query($connexion, $req);
 		$ligne = mysqli_fetch_assoc($resultat);
 		   echo afficher_titre_utilisateur($ligne['mail']);
-	   }	
+	   }
+	   
 	   if ($_GET["page"]=="supprimer_utilisateur"){
 		   if (isset($_SESSION["rank"]) && $_SESSION["rank"]==1){
 		   echo supprimer_utilisateur();
@@ -140,6 +149,7 @@
 				echo redirection();
 			}
 	   }
+	   
 	   if ($_GET["page"]=="moderation_categorie"){
 		   if (isset($_SESSION["rank"]) && $_SESSION["rank"]==1){
 		   echo moderation_categorie();
@@ -181,7 +191,7 @@
        ?>
 	   <footer>
 	<?php
-	echo afficher_footer();
+	echo afficher_footer(); // On affiche le footer a chaque page
          ?>
 </footer>
 	   <br/>
