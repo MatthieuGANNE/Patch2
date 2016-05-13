7a9b02ecbfd3000c9a30dbd1f01bbc19b1f4941c
Firalon <floszc@orange.fr>
2016-05-13 16:31:33 +0200

    Update accueil.php

--- accueil.php
+++ accueil.php
@@ -1,6 +1,6 @@
 <?php
 function accueil(){
-	if (!isset($_SESSION["nom"])){
+	if (!isset($_SESSION["nom"])){ // Si le champ nom est set, alors ils le sont tous et l'utilisateur est connecté
 		return "Veuillez vous identifier pour publier un article <a href=\"index.php?page=connexion\">Connexion</a> <a href=\"index.php?page=inscription\">Inscription</a>";
 	} else {
 		return accueil_logged($_SESSION["nom"], $_SESSION["prenom"]);
